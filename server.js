d38d320999d1948a4ae77b43e910fb12becce80c
Andrei Anashkin <anashkin.andrei@gmail.com>
2018-03-26 18:56:55 -0700

    rm server

--- server.js
+++
@@ -1,9 +0,0 @@
-var server = new Server({
-    controllers: "./app/controllers",
-    middleware: "./app/middleware",
-    autoStart: false,
-    debug: console.log,
-    cluster: 4
-});
-server.use(require('cookie-parser'));
-server.start();
